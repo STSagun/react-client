170e4796006cdc4e17728ae8facf4646d16b5e20
Sagun Saluja <sagun.saluja@successive.tech>
2019-02-26 17:49:47 +0530

    feat: React Navbar and Login Componenet

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { Typography } from '@material-ui/core';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from './theme';
 import Trainee from './pages/Trainee/Trainee';
+import Navbar from './pages/Components/Navbar/Navbar';
+import Login from './pages/Login';
 
 
 const App = () => (
   <MuiThemeProvider theme={theme}>
     <Typography>
+      <CssBaseline />
+      <Navbar />
+      {/* <Login /> */}
       <Trainee />
     </Typography>
   </MuiThemeProvider>
